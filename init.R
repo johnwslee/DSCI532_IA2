8ea34cd62b3fc09bb76aaca708329fa7186a6786
johnwslee <john.ws.lee@gmail.com>
2022-03-11 13:34:06 -0800

    Ready for heroku upload

--- init.R
+++ init.R
@@ -9,5 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes'))
+install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes', 'dashCoreComponents', 'dashHtmlComponents', 'ggplot2', 'plotly', 'gapminder', 'tidyverse', 'scales', 'devtools'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
